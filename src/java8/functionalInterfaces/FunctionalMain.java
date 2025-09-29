a922d9ed26e690e724e8a1eb59a647919a90e209
backendverse <225637829+backendverse@users.noreply.github.com>
2025-09-29 09:53:53 +0530

    Function Tutorial

--- src/java8/functionalInterfaces/FunctionalMain.java
+++ src/java8/functionalInterfaces/FunctionalMain.java
@@ -6,13 +6,56 @@ import java8.functionalInterfaces.interfaces.MyFunctionalInterface;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 public class FunctionalMain {
 
     public static void main(String[] args) {
 //        functionalInterface();
-        predicate();
+//        predicate();
+        function();
+    }
+
+    static void function() {
+//        Function<String, Integer> function = (inputString) -> {
+//            return Integer.parseInt(inputString);
+//        };
+//        System.out.println(function.apply("10"));
+
+//        Function<Integer, Integer> times2 = input -> {
+//            return input * 2;
+//        };
+//
+//        Function<Integer, Integer> square = input -> {
+//            return input * input;
+//        };
+//
+//        Function<Integer, Integer> andThen = times2.andThen(square);
+//        System.out.println("And Then Output => " + andThen.apply(3)); //36
+//
+//        Function<Integer, Integer> compose = times2.compose(square);
+//        System.out.println("Compose Output => " + compose.apply(3)); //18
+
+        List<String> outputNames = functionPredicateExecutor(
+                List.of("Henry", "Alex"),
+                (userInput) -> userInput.stream().anyMatch(user -> "Alexy".equals(user)),
+                (usersInput) -> usersInput.stream().map(String::toUpperCase).toList(),
+                (userInput) -> userInput.stream().map(String::toLowerCase).toList()
+        );
+
+        for (String outputName : outputNames) {
+            System.out.println(outputName);
+        }
+    }
+
+    static <I, R> R functionPredicateExecutor(I input,
+                                              Predicate<I> ifCondition, Function<I, R> ifFunction,
+                                              Function<I, R> elseFunction) {
+        if (ifCondition.test(input)) {
+            return ifFunction.apply(input);
+        }
+        return elseFunction.apply(input);
     }
 
     static void predicate() {
@@ -24,7 +67,9 @@ public class FunctionalMain {
         predicateCustomExecutor(
                 "Hello World",
                 (input) -> input.startsWith("H"),
-                () -> {System.out.println("This is our generic body implementation");}
+                () -> {
+                    System.out.println("This is our generic body implementation");
+                }
         );
     }
 
