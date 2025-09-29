67c5fb65409444fef4af2753d4c0a0e489498385
backendverse <225637829+backendverse@users.noreply.github.com>
2025-09-29 08:47:25 +0530

    Predicate Tutorial

--- src/java8/functionalInterfaces/FunctionalMain.java
+++ src/java8/functionalInterfaces/FunctionalMain.java
@@ -6,11 +6,43 @@ import java8.functionalInterfaces.interfaces.MyFunctionalInterface;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
 
 public class FunctionalMain {
 
     public static void main(String[] args) {
-//        UniqueConvertor convertor = new UniqueConvertor();
+//        functionalInterface();
+        predicate();
+    }
+
+    static void predicate() {
+//        Predicate<String> predicate = (input) -> {
+//            return input.toLowerCase().contains("hello");
+//        };
+//        System.out.println(predicate.test("Hello World"));
+
+        predicateCustomExecutor(
+                "Hello World",
+                (input) -> input.startsWith("H"),
+                () -> {System.out.println("This is our generic body implementation");}
+        );
+    }
+
+    static <I> void predicateCustomExecutor(I input, Predicate<I> predicate,
+                                            CustomInterface customInterface) {
+        try {
+            if (predicate.test(input)) {
+                System.out.println("Executing Generic Implementation");
+                customInterface.execute();
+                System.out.println("Generic Implementation Executed");
+            }
+        } catch (Exception e) {
+            System.out.println("Error occured : " + e);
+        }
+    }
+
+    static void functionalInterface() {
+        //        UniqueConvertor convertor = new UniqueConvertor();
 //        Set<String> ans = convertor.operate(List.of("a", "c", "c"));
         MyFunctionalInterface ans = (input) -> new HashSet<>(input);
         Set<String> operate = ans.operate(List.of("a", "c", "c"));
@@ -23,7 +55,6 @@ public class FunctionalMain {
 //            System.out.println("Custom logic 2");
         });
     }
-
 
     static void customExecutor(CustomInterface customInterface) {
         try {
