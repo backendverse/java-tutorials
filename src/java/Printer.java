c8483f5effacff729bb5cd2035a3f8c7735c74e1
backendverse <225637829+backendverse@users.noreply.github.com>
2025-11-23 13:41:08 +0530

    Core Java Code Moved in this repo

---
+++ src/java/Printer.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Printer {
+
+    public static void main(String[] args) {
+        System.out.println("Learning From BackendVerse");
+        System.out.println(args[0]);
+    }
+
+}
